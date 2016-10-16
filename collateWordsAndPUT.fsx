c72f57c6f5033529983b9adfaca71f2b5d1b7dfe
mrandri19 <andrecogno@hotmail.it>
2016-10-16 18:47:34 +0200

    Add features to fsx script and collatedWordsWithLink data file

--- collateWordsAndPUT.fsx
+++ collateWordsAndPUT.fsx
@@ -1,11 +1,9 @@
 #r @"./FSharp.Data.2.3.2/lib/net40/FSharp.Data.dll"
 #r @"./Newtonsoft.Json.6.0.5/lib/net40/Newtonsoft.Json.dll"
-#r @"../trie/lib.dll"
 let l x = printfn "%A" x
 
 open System
 open FSharp.Data
-open AndreaCognolato
 
 let clean (str: string) =
     let tmp: string = str.Trim().Split([|'('|]).[0].TrimEnd()
@@ -13,7 +11,6 @@ let clean (str: string) =
     let withoutDiacritics = String.filter (fun c -> Char.GetUnicodeCategory(c) <> Globalization.UnicodeCategory.NonSpacingMark) normal
     withoutDiacritics.ToLower()
 
-let trie = Trie.create "a"
 
 type Suggestion = {input: string[]; output: string}
 type Word = {name: string; link: string; suggest: Suggestion}
@@ -22,7 +19,31 @@ type Words = JsonProvider<"./data/file-1.json">
 
 let f word = System.Diagnostics.Process.Start("/usr/bin/curl", ("-XPUT -F 'word=" + word + "' localhost:8000/words  -H 'Content-Type: multipart/form-data;'"))
 
-for i in 0..200 do
-    let words: Words.Root[] = Words.Load (sprintf "./data/file-%d.json" i)
-    let cleanWords = Array.map (fun (word: Words.Root) -> word.Name |> clean) words
-    cleanWords |> Array.toList |> List.map f
+// PUT to localhost:8080 all of the collated words
+// for i in 0..200 do
+//     let words: Words.Root[] = Words.Load (sprintf "./data/file-%d.json" i)
+//     let cleanWords = Array.map (fun (word: Words.Root) -> word.Name |> clean) words
+//     cleanWords |> Array.toList |> List.map f |> ignore
+
+// Print to STOUT all of the collated words
+// for i in 0..1440 do
+//         let words: Words.Root[] = Words.Load (sprintf "./data/file-%d.json" i)
+//         let cleanWords = Array.map (fun (word: Words.Root) -> word.Name |> clean) words
+//         cleanWords |> Array.toList |> List.map (printfn "%s") |> ignore
+
+// Print to STOUT all of the collated words with the links
+// let cleanLink (link:string) =
+//     link.Split([|'='|]).[1]
+
+// for i in 0..1440 do
+//         let words: Words.Root[] = Words.Load (sprintf "./data/file-%d.json" i)
+//         let cleanWords = Array.map (fun (word: Words.Root) -> (word.Name |> clean, word.Link |> cleanLink)) words
+//         cleanWords |> Array.toList |> List.map (fun (w, s) -> printfn "%s %s" w s) |> ignore
+
+let cleanLink (link:string) =
+    link.Split([|'='|]).[1]
+
+for i in 0..20 do
+        let words: Words.Root[] = Words.Load (sprintf "./data/file-%d.json" i)
+        let cleanWords = Array.map (fun (word: Words.Root) -> (word.Name |> clean, word.Link |> cleanLink)) words
+        cleanWords |> Array.toList |> List.map (fun (w, s) -> (sprintf "/%s_%s" w s) |> f ) |> ignore
